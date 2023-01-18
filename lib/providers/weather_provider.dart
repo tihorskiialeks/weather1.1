0741abdfd77a276e504e9c190eb2d83726334eb4
tihorskiialeks <105417398+tihorskiialeks@users.noreply.github.com>
2023-01-18 22:02:45 +0200

    Update weather_provider.dart

--- lib/providers/weather_provider.dart
+++ lib/providers/weather_provider.dart
@@ -1,6 +1,32 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 import '../models/weather.dart';
 import '../models/custom_error.dart';
+import '../repositories/weather_repository.dart';
+
 part 'weather_state.dart';
 
+class WeatherProvider with ChangeNotifier {
+  WeatherState _state = WeatherState.initial();
+  final WeatherRepository repository;
+
+  WeatherState get state => _state;
+
+  WeatherProvider({required this.repository});
+
+  Future<void> fetchWeather(String city) async {
+    _state = _state.copyWith(status: WeatherStatus.loading);
+    notifyListeners();
+    try {
+      final Weather weather = await repository.fetchWeather(city);
+      _state = _state.copyWith(weather: weather, status: WeatherStatus.loaded);
+      //todo print(state) to check if it works properly;
+      notifyListeners();
+    } on CustomError catch (e) {
+      _state = _state.copyWith(status: WeatherStatus.error, error: e);
+      //todo print(state) to check if it works properly;
+      notifyListeners();
+    }
+  }
+}
