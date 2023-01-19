04ffa6df8d3835ef71dfa3fc6e2d9d86bfbd4961
tihorskiialeks <105417398+tihorskiialeks@users.noreply.github.com>
2023-01-19 23:04:42 +0200

    creating search page
    and navigation form homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather1/pages/search_page.dart';
 import 'package:weather1/providers/weather_provider.dart';
 import '../services/weather_api_services.dart';
 import '../repositories/weather_repository.dart';
@@ -14,12 +15,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
- 
+  String? _city;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather'),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                _city = await Navigator.push(context,
+                    MaterialPageRoute(builder: (context) {
+                  return SearchPage();
+                }));
+                print(_city);
+              },
+              icon: Icon(Icons.search)),
+        ],
       ),
       body: const Center(
         child: Text('home'),
