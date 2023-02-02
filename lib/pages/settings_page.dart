3b250810ef40825d9933df567ae52967e6817956
tihorskiialeks <105417398+tihorskiialeks@users.noreply.github.com>
2023-02-02 14:19:38 +0200

    Change stateful to stateless

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -3,14 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/provider.dart';
 
-class SettingsPage extends StatefulWidget {
+class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
-  @override
-  State<SettingsPage> createState() => _SettingsPageState();
-}
-
-class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
