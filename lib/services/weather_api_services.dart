1d423c449007670c64aa014dab027a2b5534e4b2
tihorskiialeks <105417398+tihorskiialeks@users.noreply.github.com>
2023-02-02 10:07:33 +0200

    Update weather_api_services.dart
    Unnecessary code was deleted

--- lib/services/weather_api_services.dart
+++ lib/services/weather_api_services.dart
@@ -14,53 +14,46 @@ class WeatherApiServices {
   WeatherApiServices({required this.httpClient});
 
   Future<CityCoordinates> getCityCoordinates(String city) async {
-    Uri uri = Uri(
-        scheme: 'https',
-        host: kApiHost,
-        path: '/geo/1.0/direct',
-        queryParameters: {
-          'q': city,
-          'limit': kLimit,
-          'appid': dotenv.env['APPID']
-        });
-    try {
-      final http.Response response = await httpClient.get(uri);
-      if (response.statusCode != 200) {
-        throw Exception(httpErrorHandler(response));
-      }
-      final responseBody = json.decode(response.body);
-
-      if (responseBody.isEmpty) {
-        throw WeatherException(message: 'Cannot get location of the $city');
-      }
-      final directGeocoding = CityCoordinates.fromJson(responseBody);
-      return directGeocoding;
-    } catch (e) {
-      rethrow;
+    final http.Response response = await httpClient.get(
+      Uri(
+          scheme: 'https',
+          host: kApiHost,
+          path: '/geo/1.0/direct',
+          queryParameters: {
+            'q': city,
+            'limit': kLimit,
+            'appid': dotenv.env['APPID']
+          }),
+    );
+    if (response.statusCode != 200) {
+      throw Exception(httpErrorHandler(response));
     }
+    final responseBody = json.decode(response.body);
+
+    if (responseBody.isEmpty) {
+      throw WeatherException(message: 'Cannot get location of the $city');
+    }
+    return CityCoordinates.fromJson(responseBody);
   }
 
   Future<Weather> getWeather(CityCoordinates directGeocoding) async {
-    final Uri uri = Uri(
-      scheme: 'https',
-        path: '/data/2.5/weather',
-        host: kApiHost,
-        queryParameters: {
-      'lat': '${directGeocoding.lat}',
-      'lon': '${directGeocoding.lon}',
-      'units': kUnit,
-      'appid': dotenv.env['APPID'],
-    });
-    try {
-      final http.Response response = await httpClient.get(uri);
-      if (response.statusCode != 200) {
-        throw Exception(httpErrorHandler(response));
-      }
-      final weatherJson = json.decode(response.body);
-      final Weather weather = Weather.fromJson(weatherJson);
-      return weather;
-    } catch (e) {
-      rethrow;
+    final http.Response response = await httpClient.get(
+      Uri(
+          scheme: 'https',
+          path: '/data/2.5/weather',
+          host: kApiHost,
+          queryParameters: {
+            'lat': '${directGeocoding.lat}',
+            'lon': '${directGeocoding.lon}',
+            'units': kUnit,
+            'appid': dotenv.env['APPID'],
+          }),
+    );
+    if (response.statusCode != 200) {
+      throw Exception(httpErrorHandler(response));
     }
+    final weatherJson = json.decode(response.body);
+    final Weather weather = Weather.fromJson(weatherJson);
+    return weather;
   }
 }
